e127f035e2c182cdc09585e2c554dcb4932f29df
Ayu-web-cpu <ayushsingh33695@gmail.com>
2025-06-19 13:55:33 +0530

    Update loan_approval_webapp.py

--- loan_approval_webapp.py
+++ loan_approval_webapp.py
@@ -1,21 +1,15 @@
 import streamlit as st
 import numpy as np
 import joblib
-
-# Load model, scaler, encoder
 rf = joblib.load('rf_model.pkl')
 scaler = joblib.load('scaler.pkl')
 le = joblib.load('education_encoder.pkl')
-
 st.title("Bank Loan Approval Predictor")
-
 st.write("Enter applicant details:")
-
 age = st.slider("Age", 21, 60, 30)
 income = st.number_input("Annual Income", min_value=20000, max_value=100000000, value=50000, step=1000)  # Up to 10 crore
 education = st.selectbox("Education", ['Graduate', 'Not Graduate'])
 credit_score = st.slider("Credit Score", 300, 900, 600)
-
 if st.button("Predict Loan Approval"):
     education_encoded = le.transform([education])[0]
     X_new = np.array([[age, income, education_encoded, credit_score]])
